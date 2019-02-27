e1a01943d4b572f696cd303e0f3ceb9cf0a63645
Mathias Lohne <mathialo@ifi.uio.no>
2019-02-27 15:14:17 +0100

    Bug in install script

--- install_linux.sh
+++ install_linux.sh
@@ -8,8 +8,6 @@ fi
 echo "Copying files"
 mkdir -p /usr/local/share/texmf/tex/latex/signlines
 cp signlines.sty /usr/local/share/texmf/tex/latex/signlines
-echo "Updating package index"
-cd /usr/local/share/texmf/ && mktexlsr > /dev/null
 
 read -p "Are you using TeXstudio? [y/n] " yn
 if [[ "$yn" == "y" || "$yn" == "Y" ]]; then
@@ -18,5 +16,8 @@ if [[ "$yn" == "y" || "$yn" == "Y" ]]; then
 	cp signlines.cwl ~/.config/texstudio/completion/user
 	chmod 644 ~/.config/texstudio/completion/user/signlines.cwl
 fi
+
+echo "Updating package index"
+cd /usr/local/share/texmf/ && mktexlsr > /dev/null
 
 echo "Installation done"
